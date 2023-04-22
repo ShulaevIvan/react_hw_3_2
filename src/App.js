f1e2a190727ce6544d85ec4afa00417b7ad5a1b3
Shulaev Ivan <mail@ntdservice.ru>
2023-04-22 16:25:59 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import './components/Listing/Listing.css';
+import Listing from './components/Listing/Listing';
+import jsonData from './data/etsy.json';
 
 function App() {
+  const  data = jsonData;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Listing items={data}></Listing>
     </div>
   );
 }
---
+++ src/components/Listing/Listing.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Listing = (props) => {
+    return (
+        <React.Fragment>
+            <div className="item-list">
+            {props.items.map((item, i) => {
+                return (
+                    <React.Fragment key={item.listing_id}>
+                        <div className="item" id = {item.id}>
+                            <div className="item-image">
+                                <a key={item.listing_id} href={item.url}>
+                                    <img src={item.MainImage ? item.MainImage.url_570xN: ''} alt={item.title}/>
+                                </a>
+                            </div>
+                            <div className="item-details">
+                                <p className="item-title">{item.title && item.title.length > 50 ? `${item.title.substring(0, 50)}...`: item.title}</p>
+                                <p className="item-price">{item.currency_code} {item.price}</p>
+                                <p className="item-quantity level-medium">{item.quantity} left</p>
+                            </div>
+                        </div>
+                    </React.Fragment>
+                );
+            })}
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default Listing;
