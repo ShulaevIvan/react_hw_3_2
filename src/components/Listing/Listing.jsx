98e08ecb3c85785245fd8efb37c40ca1b6ef8268
Shulaev Ivan <mail@ntdservice.ru>
2023-04-25 01:30:28 +0300

    fix

--- src/components/Listing/Listing.jsx
+++ src/components/Listing/Listing.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
 const Listing = (props) => {
+    let qntLevelClass = undefined;
     return (
         <React.Fragment>
             <div className="item-list">
             {props.items.map((item, i) => {
+                
+                if (item.quantity <= 10)  qntLevelClass = 'item-quantity level-low';
+                else if (item.quantity !== 10 && item.quantity <= 20) qntLevelClass = 'item-quantity level-medium';
+                else qntLevelClass = 'item-quantity level-high';
+                
                 return (
                     <React.Fragment key={item.listing_id}>
                         <div className="item" id = {item.id}>
@@ -16,7 +22,8 @@ const Listing = (props) => {
                             <div className="item-details">
                                 <p className="item-title">{item.title && item.title.length > 50 ? `${item.title.substring(0, 50)}...`: item.title}</p>
                                 <p className="item-price">{item.currency_code} {item.price}</p>
-                                <p className="item-quantity level-medium">{item.quantity} left</p>
+                                <p className={qntLevelClass}>{item.quantity} left</p>
+                                {/* <p className="item-quantity level-medium">{item.quantity} left</p> */}
                             </div>
                         </div>
                     </React.Fragment>
